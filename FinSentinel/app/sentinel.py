cffccde79b8f75d7e9549c578197822c2b0f75e8
KaloqnS <arhangel9808@gmail.com>
2025-07-09 20:33:01 +0300

    - Cleaned a little sentinel.py code - Added Coinbase site api for bitcoin prices to the sentinel.py - Added code comparison all to binance , and now i started adding bitfinex vs code comparisons, - changed the logging and logs code to be easier to read  - removed the exe build , will build it when all the code I want to be in the app is coded, building the exe will be the last step

--- FinSentinel/app/sentinel.py
+++ FinSentinel/app/sentinel.py
@@ -2,7 +2,7 @@ import threading
 import time
 import requests
 
-from app.shared_queue import price_diff_queue
+# from app.shared_queue import price_diff_queue, this module is for real time data to be sent to a graph in the main.py file as we can add application graph there
 from app.logger import logger
 from app.alerts import send_slack_alert, send_exchange_rate_alert
 
@@ -21,17 +21,25 @@ def fetch_usd_to_eur_rate():
     except Exception:
         logger.exception("Failed to fetch USD to EUR exchange rate")
         return None
+# fetch Bitcoin price from Coinbase Exchange
+def fetch_price_coinbase_bitcoin():
+    url = "https://api.exchange.coinbase.com/products/BTC-EUR/ticker"
+    data = requests.get(url).json()
+    logger.info("Coinbase API response: %s", data)
+    return float(data["price"])
 
 # fetch Bitcoin price from Binance
 def fetch_price_binance_bitcoin():
     url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCEUR"
     data = requests.get(url).json()
+    logger.info("Binance API response: %s", data)
     return float(data["price"])
 
 # fetch Bitcoin price from Bitfinex
 def fetch_price_bitfinex_bitcoin():
     url = "https://api-pub.bitfinex.com/v2/ticker/tBTCEUR"
     data = requests.get(url).json()
+    logger.info("Bitfinex API response: %s", data)
     return float(data[6])  # Last price
 
 # fetch Bitcoin price from Kraken
@@ -93,7 +101,7 @@ def fetch_price_bitget_bitcoin():
         response = requests.get(url, timeout=5)
         data = response.json()
 
-        logger.info("Bitget API response: %s", data)
+        #logger.info("Bitget API response: %s", data) - commenting out for now as the volume of the api response is large and can clutter logs
 
         if data.get("code") != "00000":
             raise ValueError(f"Bitget API returned error code: {data.get('code')}")
@@ -127,23 +135,52 @@ def fetch_price_bitget_bitcoin():
 def monitor_loop():
     global running, exchange_rate_alert_sent
     while running:
+        # ============= Bitcoin, Binance vs =============
         # Bitcoin,Binance vs bitfinex
         try:
             price_binance = fetch_price_binance_bitcoin()
             price_bitfinex = fetch_price_bitfinex_bitcoin()
 
+            if price_bitfinex is None:
+                logger.info("Skipping Bitcoin = Binance/Bitfinex comparison due to failed price fetch.")
+                continue
+
             avg = (price_binance + price_bitfinex) / 2
             diff = abs(price_binance - price_bitfinex)
             percent_diff = (diff / avg) * 100
 
-            logger.info(f"Binance: {price_binance:.6f}, Bitfinex: {price_bitfinex:.6f}, Diff: {percent_diff:.8f}%")
-            price_diff_queue.put(("Binance vs Bitfinex", percent_diff, 0))
+            logger.info(f"Bitcoin = Binance: {price_binance:.6f}, Bitfinex: {price_bitfinex:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Binance vs Bitfinex", percent_diff, 0)) - this is needed if the import queue module is enabled
 
             if percent_diff >= 0.00002:
                 send_slack_alert("Bitcoin", "Binance vs Bitfinex", price_binance, price_bitfinex, percent_diff)
 
         except Exception:
-            logger.exception("Error comparing Bitcoin,Binance vs Bitfinex")
+            logger.exception("Error comparing Bitcoin = Binance vs Bitfinex")
+            if not running:
+                break
+            time.sleep(5)
+        # Bitcoin,Binance vs Coinbase
+        try:
+            price_binance = fetch_price_binance_bitcoin()
+            price_coinbase = fetch_price_coinbase_bitcoin()
+
+            if price_coinbase is None:
+                logger.info("Skipping Bitcoin = Binance/Coinbase comparison due to failed price fetch.")
+                continue
+
+            avg = (price_binance + price_coinbase) / 2
+            diff = abs(price_binance - price_coinbase)
+            percent_diff = (diff / avg) * 100
+
+            logger.info(f"Bitcoin = Binance: {price_binance:.6f}, Coinbase: {price_coinbase:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Binance vs Coinbase", percent_diff, 0)) - this is needed if the import queue module is enabled
+
+            if percent_diff >= 0.00002:
+                send_slack_alert("Bitcoin", "Binance vs Coinbase", price_binance, price_coinbase, percent_diff)
+
+        except Exception:
+            logger.exception("Error comparing Bitcoin = Binance vs Coinbase")
             if not running:
                 break
             time.sleep(5)
@@ -153,22 +190,22 @@ def monitor_loop():
             price_binance = fetch_price_binance_bitcoin()
             price_kraken = fetch_price_kraken_bitcoin()
 
-            if price_binance is None or price_kraken is None:
-                logger.info("Skipping Bitcoin, Binance/Kraken comparison due to failed price fetch.")
+            if price_kraken is None:
+                logger.info("Skipping Bitcoin = Binance/Kraken comparison due to failed price fetch.")
                 continue
 
             avg = (price_binance + price_kraken) / 2
             diff = abs(price_binance - price_kraken)
             percent_diff = (diff / avg) * 100
 
-            logger.info(f"Binance: {price_binance:.6f}, Kraken: {price_kraken:.6f}, Diff: {percent_diff:.8f}%")
-            price_diff_queue.put(("Binance vs Kraken", percent_diff, 0))
+            logger.info(f"Bitcoin = Binance: {price_binance:.6f}, Kraken: {price_kraken:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Binance vs Kraken", percent_diff, 0)) - this is needed if the import queue module is enabled
 
             if percent_diff >= 0.00002:
                 send_slack_alert("Bitcoin", "Binance vs Kraken", price_binance, price_kraken, percent_diff)
 
         except Exception:
-            logger.exception("Error comparing Bitcoin,Binance vs Kraken")
+            logger.exception("Error comparing Bitcoin = Binance vs Kraken")
             if not running:
                 break
             time.sleep(5)
@@ -179,21 +216,21 @@ def monitor_loop():
             price_crypto_exchange = fetch_price_crypto_exchange_bitcoin()
 
             if price_crypto_exchange is None:
-                logger.info("Skipping Bitcoin, Binance/Crypto.com comparison due to failed price fetch.")
+                logger.info("Skipping Bitcoin = Binance/Crypto.com comparison due to failed price fetch.")
                 continue
 
             avg = (price_binance + price_crypto_exchange) / 2
             diff = abs(price_binance - price_crypto_exchange)
             percent_diff = (diff / avg) * 100
 
-            logger.info(f"Binance: {price_binance:.6f}, Crypto Exchange: {price_crypto_exchange:.6f}, Diff: {percent_diff:.8f}%")
-            price_diff_queue.put(("Binance vs Crypto", percent_diff, 0))
+            logger.info(f"Bitcoin = Binance: {price_binance:.6f}, Crypto Exchange: {price_crypto_exchange:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Binance vs Crypto", percent_diff, 0)) - this is needed if the import queue module is enabled
 
             if percent_diff >= 0.00002:
                 send_slack_alert("Bitcoin", "Binance vs Crypto", price_binance, price_crypto_exchange, percent_diff)
 
         except Exception:
-            logger.exception("Error comparing Bitcoin, Binance vs Crypto.com")
+            logger.exception("Error comparing Bitcoin = Binance vs Crypto.com")
             if not running:
                 break
             time.sleep(5)
@@ -207,7 +244,7 @@ def monitor_loop():
                 if not exchange_rate_alert_sent:
                     send_exchange_rate_alert()
                     exchange_rate_alert_sent = True
-                logger.info("Skipping Bitcoin, Binance/Bitget comparison due to failed price or exchange rate fetch.")
+                logger.info("Skipping Bitcoin = Binance/Bitget comparison due to failed price or exchange rate fetch.")
                 continue
 
             exchange_rate_alert_sent = False
@@ -216,13 +253,65 @@ def monitor_loop():
             diff = abs(price_binance - price_bitget)
             percent_diff = (diff / avg) * 100
 
-            logger.info(f"Binance: {price_binance:.6f}, Bitget: {price_bitget:.6f}, Diff: {percent_diff:.8f}%")
-            price_diff_queue.put(("Binance vs Bitget", percent_diff, 0))
+            logger.info(f"Bitcoin = Binance: {price_binance:.6f}, Bitget: {price_bitget:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Binance vs Bitget", percent_diff, 0)) - this is needed if the import queue module is enabled
 
             if percent_diff >= 0.00002:
                 send_slack_alert("Bitcoin", "Binance vs Bitget", price_binance, price_bitget, percent_diff)
         except Exception:
-            logger.exception("Error comparing Bitcoin, Binance vs Bitget")
+            logger.exception("Error comparing Bitcoin = Binance vs Bitget")
+            if not running:
+                break
+            time.sleep(5)
+
+             # ============= Bitcoin, Bitfinex vs =============
+             # Bitcoin,Bitfinex vs Crypto.com
+        try:
+            price_bitfinex = fetch_price_bitfinex_bitcoin()
+            price_crypto_exchange = fetch_price_crypto_exchange_bitcoin()
+
+            if price_crypto_exchange is None:
+                logger.info("Skipping Bitcoin = Bitfinex/Crypto comparison due to failed price fetch.")
+                continue
+
+            avg = (price_crypto_exchange + price_bitfinex) / 2
+            diff = abs(price_bitfinex - price_crypto_exchange)
+            percent_diff = (diff / avg) * 100
+
+            logger.info(f"Bitcoin = Bitfinex: {price_bitfinex:.6f}, Crypto: {price_crypto_exchange:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Bitfinex vs Crypto", percent_diff, 0)) - this is needed if the import queue module is enabled
+
+            if percent_diff >= 0.00002:
+                send_slack_alert("Bitcoin", "Bitfinex vs Crypto", price_bitfinex, price_crypto_exchange, percent_diff)
+
+        except Exception:
+            logger.exception("Error comparing Bitcoin = Bitfinex vs Crypto")
+            if not running:
+                break
+            time.sleep(5)
+
+
+            # Bitcoin,Bitfinex vs Coinbase
+        try:
+            price_bitfinex = fetch_price_bitfinex_bitcoin()
+            price_coinbase = fetch_price_coinbase_bitcoin()
+
+            if price_coinbase is None:
+                logger.info("Skipping Bitcoin = Bitfinex/Crypto comparison due to failed price fetch.")
+                continue
+
+            avg = (price_coinbase + price_bitfinex) / 2
+            diff = abs(price_bitfinex - price_coinbase)
+            percent_diff = (diff / avg) * 100
+
+            logger.info(f"Bitcoin = Bitfinex: {price_bitfinex:.6f}, Coinbase: {price_coinbase:.6f}, Diff: {percent_diff:.8f}%")
+           # price_diff_queue.put(("Bitfinex vs Coinbase", percent_diff, 0)) - this is needed if the import queue module is enabled
+
+            if percent_diff >= 0.00002:
+                send_slack_alert("Bitcoin", "Bitfinex vs Coinbase", price_bitfinex, price_coinbase, percent_diff)
+
+        except Exception:
+            logger.exception("Error comparing Bitcoin = Bitfinex vs Coinbase")
             if not running:
                 break
             time.sleep(5)
